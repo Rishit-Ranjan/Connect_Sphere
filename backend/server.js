aff38772a7c790faa4743c285c66f12b2c599e07
Rishit Ranjan <rishitranjan131@gmail.com>
2026-07-02 10:44:43 +0530

    refactor: update server.js to use ES module syntax for app and database connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const app= require('./src/app');
-const connectDB= require('./src/config/db');
+import { listen } from './src/app';
+import connectDB from './src/config/db';
 
 const PORT= process.env.PORT|| 5000;
 
 connectDB().then(()=> {
-    app.listen(PORT, ()=> {
+    listen(PORT, ()=> {
         console.log(`Server running on port ${PORT}`);
     });
 });
